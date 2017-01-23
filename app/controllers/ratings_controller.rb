041e3aee2cb35187f9cd7b02e489945499772111
Topi Kiviluoma <topi.kiviluoma@helsinki.fi>
2017-01-23 16:10:45 +0200

    Viikon 2 tehtäviä 1-5

---
+++ app/controllers/ratings_controller.rb
@@ -0,0 +1,7 @@
+class RatingsController < ApplicationController
+  def index
+    @ratings = Rating.all
+
+  end
+
+end
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,12 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
+  has_many :ratings
+
+  def average_rating
+    sum = 0
+    ratings.each do |rating|
+      sum = sum + rating.score
+    end
+    return sum/ratings.count
+  end
 end
