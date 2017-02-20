447b6c023c3c5e349f5a4b74a6c10d86b5db21af
Topi Kiviluoma <topi.kiviluoma@helsinki.fi>
2017-02-20 16:37:05 +0200

    fixed weather on bars

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,14 +4,16 @@ class PlacesController < ApplicationController
   end
 
   def search
-    @places = BeermappingApi.places_in(params[:city])
+    city = params[:city]
+    @places = BeermappingApi.places_in(city)
+    @weather = WeatherService.weather_for(city)
     if @places.empty?
       redirect_to places_path, notice: "No locations in #{params[:city]}"
     else
       session[:last_city] = params[:city]
       render :index
     end
-    @weather = WeatherService.weather_for(params[:city])
+
   end
   def show
     places_in_cache = Rails.cache.read(session[:last_city])
